f4d848c41df2672f095c4da5c903473874e9d34f
Solar <redflare31138@gmail.com>
2026-03-29 20:52:08 +0700

    menambahkan class DataGeneric

--- TP_MODUL5_103022400141/Program.cs
+++ TP_MODUL5_103022400141/Program.cs
@@ -1,2 +1,23 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+class DataGeneric<Type>
+{
+    private Type data;
+    public DataGeneric(Type data)
+    {
+        this.data = data;
+    }
+    public void PrintData()
+    {
+        Type Y = this.data;
+        Console.WriteLine("Data yang tersimpan adalah: " + Y);
+    }
+}
+public class main
+{
+    public static void Main(string[] args)
+    {
+        DataGeneric<long> dataInt = new DataGeneric<long>(103022400141);
+        
+        dataInt.PrintData();
+    }
+}
