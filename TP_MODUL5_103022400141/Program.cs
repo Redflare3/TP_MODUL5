61f055b934a601b4d963900509eec6f48ac3e8cc
Solar <141292528+Redflare3@users.noreply.github.com>
2026-03-29 22:14:43 +0700

    Fix merge conflict

--- TP_MODUL5_103022400141/Program.cs
+++ TP_MODUL5_103022400141/Program.cs
@@ -1,23 +1,29 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System;
+
 class DataGeneric<Type>
 {
     private Type data;
-    public DataGeneric(Type data)
-    {
-        this.data = data;
-    }
-    public void PrintData()
-    {
-        Type Y = this.data;
-        Console.WriteLine("Data yang tersimpan adalah: " + Y);
+    public DataGeneric(Type data) { 
+        this.data = data; }
+    public void PrintData(){
+        Console.WriteLine("Data yang tersimpan adalah: " + this.data);
     }
 }
+
+class HaloGeneric
+{
+    public static void SapaUser<Type>(Type X){
+        Console.WriteLine("Halo User: " + X);
+    }
+}
+
 public class main
 {
-    public static void Main(string[] args)
-    {
+    public static void Main(string[] args){
         DataGeneric<long> dataInt = new DataGeneric<long>(103022400141);
-        
         dataInt.PrintData();
+
+        HaloGeneric.SapaUser<string>("Jiro");
     }
 }
